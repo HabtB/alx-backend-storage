30577ee4de2068ffa2a115eefbc316570a67db90
HabtB <email.Habtamu@gmail.com>
2023-10-19 23:50:53 +0000

    web.py updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,15 +1,21 @@
+#!/usr/bin/python3
+
 import requests
 import redis
 import time
 from functools import wraps
 
 
-# Initialize Redis client
+""" Initialize Redis client """
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+
 def count_accesses(method):
+    """ counts the accesses"""
     @wraps(method)
     def wrapper(url):
+        """This is a wrapper
+        """
         # Increment the count for the accessed URL
         url_key = f"count:{url}"
         redis_client.incr(url_key)
@@ -25,9 +31,11 @@ def count_accesses(method):
 
     return wrapper
 
+
 @count_accesses
 def get_page(url):
-    # Check if the content is cached
+    """ Check if the content is cached
+    """
     cache_key = f"cache:{url}"
     cached_content = redis_client.get(cache_key)
 
@@ -40,9 +48,12 @@ def get_page(url):
 
     return content
 
+
 if __name__ == "__main__":
     # Example usage of get_page function
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
+    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https:
+        //www.example.com"
     html_content = get_page(url)
-    print(f"HTML content for {url} (accessed {redis_client.get(f'count:{url}').decode('utf-8')} times):\n{html_content}")
+    print(f"HTML content for {url}(accessed {redis_client.
+        get(f'count:{url}').decode('utf-8')} times):\n{html_content}")
 
